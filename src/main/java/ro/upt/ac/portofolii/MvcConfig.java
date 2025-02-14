a263840c0df9aa0979efb3e42e49b81dd87b35e5
Christi <christianalexandrudragoi@gmail.com>
2025-02-14 20:15:01 +0200

    new style working on index and login pages

--- src/main/java/ro/upt/ac/portofolii/MvcConfig.java
+++ src/main/java/ro/upt/ac/portofolii/MvcConfig.java
@@ -1,17 +1,15 @@
 package ro.upt.ac.portofolii;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class MvcConfig implements WebMvcConfigurer 
-{
+public class MvcConfig implements WebMvcConfigurer {
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) 
-    {
-        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .setCachePeriod(3600);
     }
 }
